32629c4c44afe49884f22ebc88949cbc33647934
himanshu021213 <hrautela626@gmail.com>
2025-06-28 05:31:11 +0000

    01_variables

---
+++ 01_basics/01.variables.js
@@ -0,0 +1,16 @@
+const accountId =67765
+let accountEmail = "Hitesh@googole.com"
+var accountPassward = "1234"
+accountCity ="Jaipur"
+
+accountEmail ="hchi@.com"
+accountPassward ="121212121"
+accountCity ="Bengluru"
+
+/*
+prefer not to use var 
+bcz of isssue in block scope and function scope
+*/
+
+console.log(accountEmail,accountPassward,accountCity);
+
