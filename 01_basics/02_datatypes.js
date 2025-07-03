6f53045f68703c1b3d8844a9b94533130e4bff0f
himanshu021213 <hrautela626@gmail.com>
2025-07-03 03:56:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"  //treat all js code as newer verson
+
+//alert("hello")  // we are using nodejs ,not brouser
+
+console.log(3+3) 
+
+// code readability should be high
+
+console.log("himanshu")
+
+let name = "himanshu"
+let age = 20
+let inLoggedIn = false
+let state = null
+
+
+// number => 2 to power 53
+//bigint
+//string => " ",' '
+//Boolean = true/false
+// null => standalone value
+//undifined
+//symbol => unique
+
+
+// object
+
+console.log(typeof "himanshu");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
