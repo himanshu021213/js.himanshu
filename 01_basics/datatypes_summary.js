37a0d799d8b859306c44b7b78407bf0bdad1b3cf
himanshu021213 <hrautela626@gmail.com>
2025-07-06 06:56:06 +0000

    datatype-summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,40 @@
+// premitive datatypes
+
+// 7 types : String, Number , Boolean , null ,undefined ,Symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedInn= false
+const outsideTemp =null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id ===anotherId);
+
+//const bigNumbers = 346645667677666767654n
+
+
+
+
+
+// Reference(Non primitive)
+
+// Array , Objects , Functions
+
+const heros = ['Thor', 'Ironman', 'Hulk']
+let myObj ={
+    name: 'Thor',
+    age: 10,
+
+}
+
+ const myfunction =function(){
+console.log("Hello World");
+
+}
+
+console.log(typeof bigNumbers);
+console.log(typeof myfunction);
+console.log(typeof anotherId);
