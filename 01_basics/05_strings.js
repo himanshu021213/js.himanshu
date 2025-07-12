6ea71ebaf6587db91e85f9cee028362e0aa5524f
himanshu021213 <hrautela626@gmail.com>
2025-07-12 16:15:15 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const name ="himanshu"
+const repoCount = 50
+
+//console.log(name + repoCount + "Value");                // old synatx use new syntax instead.
+
+console.log('Hello my name is ${name} and my repo count is $ {repoCount}');
+
+
+const gameName = new String('himanshu')
