0f518e5a5f79234c3974b62f97507223c553ef5c
himanshu021213 <hrautela626@gmail.com>
2025-07-13 06:05:54 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -7,3 +7,26 @@ console.log('Hello my name is ${name} and my repo count is $ {repoCount}');
 
 
 const gameName = new String('himanshu')
+
+console.log(gameName [0]);
+console.log(gameName .__proto__);
+
+
+//console.log(gameName.length);
+//console.log(gameName.toLocaleUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('m'));
+
+ const newString = gameName.substring(0, 4)
+ console.log(newString);
+
+ const anotherString = gameName.slice(-7, 6)
+ console.log(anotherString);
+ 
+ 
+ const newStringOne = "       himanshu     "
+
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
