f9f21ee823340f845b43c729c92d5d877c782988
himanshu021213 <hrautela626@gmail.com>
2025-07-14 16:26:10 +0000

    date

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,12 @@
+// dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2025, 0 ,25 , 5 ,3 )
+console.log(myCreatedDate.toLocaleString());
+
