2deb1f6f8a03945ec1107c763fcf87bd0b9b4561
himanshu021213 <hrautela626@gmail.com>
2025-07-15 02:36:11 +0000

    date

--- 01_basics/07_datesin.js
+++ 01_basics/07_datesin.js
@@ -7,6 +7,26 @@
 // console.log(typeof myDate);
 
 
-let myCreatedDate = new Date(2025, 0 ,25 , 5 ,3 )
-console.log(myCreatedDate.toLocaleString());
+//let myCreatedDate = new Date(2025, 0 ,25 , 5 ,3 )
+let myCreatedDate = new Date("01-14-2025")
+//console.log(myCreatedDate.toLocaleString());
 
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log( myCreatedDate.getTime());
+//console.log( Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getHours());
+console.log(newDate.getMinutes());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+    timeZone: ''
+})
