6efa819e225b0842b12d3a03daa8d76243b6e5be
himanshu021213 <hrautela626@gmail.com>
2025-07-17 06:25:03 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -7,8 +7,43 @@ const myHeros =["shaktiman", "naagraj"]
 const myArr2 = new Array(1, 2, 3, 4)
 
 
-console.log(myArr[0]);
-console.log(myHeros[0]);
+//console.log(myArr[0]);
+//console.log(myHeros[0]);
 
 
 // array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr);
+
+
+//myArr.unshift(9)
+//myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr =myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
+
+
+
+// slics, splics
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+
+
+const myn2 = myArr.splice(1,3)
+console.log("C", myArr);
+
+console.log(myn2);
+
