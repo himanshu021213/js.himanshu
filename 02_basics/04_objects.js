2ed5c1bcf72d2822887dcfba9f321cedbead8dbf
himanshu021213 <hrautela626@gmail.com>
2025-07-18 14:19:55 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,19 @@
+// const tinderUser =new Object()
+const tinderUser = {}
+
+tinderUser.id ="123abc"
+tinderUser.name ="sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularuser = {
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"himanshu",
+            lastname:"rautela"
+        }
+    }
+}
+// console.log(regularuser.fullname?.userfullname.firstname);
