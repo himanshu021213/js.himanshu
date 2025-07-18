ffe61dd5af3c5bd54be5abd2387d31ce04f45e62
himanshu021213 <hrautela626@gmail.com>
2025-07-18 03:30:59 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,22 @@
+// singleton
+// Object.create
+
+
+//object literals
+
+
+
+
+const JsUser = {
+    name:"Himanshu",
+    "full name": "himanshu rautela",
+    age:18,
+    location: "uttrakhand",
+    email:"himanshu@gmail.com",
+    isLOggedIn: false,
+    lastLoginDays: ["Monday","Saterday"]
+}
+
+console.log(JsUser.name);
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
