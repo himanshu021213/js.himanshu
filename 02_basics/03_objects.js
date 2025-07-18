4e0f90f4ff5287386646ba446748398a45473f4a
himanshu021213 <hrautela626@gmail.com>
2025-07-18 06:29:16 +0000

    object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -14,7 +14,7 @@ const mySym = Symbol("key1")
 const JsUser = {
     name:"Himanshu",
     "full name": "himanshu rautela",
-    mySym: "mykey1",
+    [mySym]: "mykey1",
     age:18,
     location: "uttrakhand",
     email:"himanshu@gmail.com",
@@ -22,8 +22,23 @@ const JsUser = {
     lastLoginDays: ["Monday","Saterday"]
 }
 
-console.log(JsUser.name);
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
-console.log(typeof JsUser.mySym);
+// console.log(JsUser.name);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser [mySym]);
+
+
+JsUser.email = "babbu@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hima@micro.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello Js user");
+    console.log(`Hello Js user, ${this.name}`);
+    
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greeting);
 
