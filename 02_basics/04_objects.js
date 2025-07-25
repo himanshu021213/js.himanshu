eaf2fd3d20589db19869ec3a81e7965d40864135
himanshu021213 <hrautela626@gmail.com>
2025-07-25 11:36:21 +0530

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -17,3 +17,46 @@ const regularuser = {
     }
 }
 // console.log(regularuser.fullname?.userfullname.firstname);
+
+
+
+
+const obj1 ={1: "a", 2: "b"}
+const obj2 ={3: "a", 4: "b"}
+const obj4 ={5: "a", 6: "b"}
+
+// const obj3 ={ obj1, obj2}
+// const obj3 = Object.assign({},obj1, obj2, obj4)
+
+const obj3 ={...obj1, ...obj2}
+console.log(obj3);
+
+
+// const users =[
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     }
+// ]
+
+// users[1].email
+// console.log(Object.users());
+
+
+const course ={
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "himanshu"
+
+}
+ course.courseInstructor
+
+const {courseInstructor: instructor}= course
+// console.log(courseInstructor);
+console.log(instructor);
+
+const navbar = () => {
+
+}
+
+   navbar(company ="himanshu")
